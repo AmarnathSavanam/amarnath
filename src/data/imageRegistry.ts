6927da5b820e3a8f00b02616469848ce79bbebef
AmarnathSavanam <savanamamarnathpolireddy@gmail.com>
2026-06-24 18:21:05 +0530

    Update imageRegistry.ts

--- src/data/imageRegistry.ts
+++ src/data/imageRegistry.ts
@@ -7,6 +7,10 @@ interface ImageEntry {
   banner?: string;
 }
 const imageRegistry: Record<string, ImageEntry> = {
+  "under-the-dome": {
+  poster: "/under-the-dome.jpg",
+  banner: "/under-the-dome-banner.jpg"
+},
   "scam-2003-the-telgi-story": {
   poster: "/scam-2003-the-telgi-story.jpg",
   banner: "/scam-2003-the-telgi-story-banner.jpg"
